462c4d7657ad5f0e394deeaa9997b612e7e83cf4
Bryan <xbbryan@yahoo.com.tw>
2018-10-06 21:03:51 +0800

    計算每筆留言平均長度

--- readReviews.py
+++ readReviews.py
@@ -6,7 +6,9 @@ with open('reviews.txt','r') as f:
         count += 1
         if count % 2000 == 0:
             print(len(data))
-print(len(data))
-print(data[0])
-print('-----------------------------------')
-print(data[1])
+print('檔案讀取完畢，總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+print('每筆留言平均長度為', sum_len/len(data))
